1f1e98ce526416564fdda184df5cfdf77d4571dd
MD Asif Hasan <md.asif.hasan@vanderbilt.edu>
2017-10-19 09:02:49 -0500

    image arch improved to answer query2 successfully

--- ImageryArchitecture.py
+++ ImageryArchitecture.py
@@ -72,23 +72,9 @@ class ImageryArch:
         print("Constructing imagery arch")
         self.stm = ShortTermMemory()
 
-    # find all the objects other than obj1 in the image that has a relation with obj1
-    # and extend the front with these objects and their relations with obj1
-    def extendFrontier(self, obj1_label, image, front, relations):
-        print("extend front with obj:", obj1_label, len(front))
-        obj1 = image.objects[obj1_label]
-        for r in obj1.relations:
-            if obj1.relations[r] is not None:
-                print("extending frontier with rel:" , r , "-", obj1.relations[r].obj_label)
-                front.append((obj1.relations[r], r, list(relations)))
+    def queryRelation(self, obj1, obj2, target_relation):
 
-        print("after extending the front: ", len(front))
-        for (object, relation, relations) in front:
-            print(obj1_label , relation , object.obj_label)
-
-    def query(self, obj1, obj2):
-
-        print("\n\nQuerying relation between", obj1, obj2)
+        print("\n\nQuerying relation", target_relation,"between", obj1, obj2)
 
         relations = []
         front = []
@@ -99,19 +85,12 @@ class ImageryArch:
             if image in explored:
                 continue
             explored.append(image)
-            self.extendFrontier(obj2, image, front, relations)
+            self.extendFrontier(obj2, image, front, target_relation)
 
         while len(front) > 0:
-            (object, relation, relations) = front.pop()
+            object = front.pop()
             if object.obj_label == obj1:
-                if relation not in relations:
-                    relations.append(relation)
-                print("Relation between ", obj1, "and", obj2)
-                if len(relations) == 0:
-                    print("I don't know.")
-                else:
-                    for r in relations:
-                        print(r, "= true")
+                return True
             else:
                 images = self.stm.imagesByObj[object.obj_label]
                 print("#images of ", object.obj_label, len(images))
@@ -119,13 +98,43 @@ class ImageryArch:
                     if image in explored:
                         continue
                     explored.append(image)
-                    ra = list(relations)
-                    if relation not in ra:
-                        ra.append(relation)
-                    self.extendFrontier(object.obj_label, image, front, ra)
+                    self.extendFrontier(object.obj_label, image, front, target_relation)
+        return False
 
+    # find all the objects other than obj1 in the image that has a relation with obj1
+    # and extend the front with these objects and their relations with obj1
+    def extendFrontier(self, obj1_label, image, front, target_relation):
+        print("extend front with obj:", obj1_label, len(front)), "for target_relation:", target_relation
+        obj1 = image.objects[obj1_label]
+        if target_relation in obj1.relations:
+            if obj1.relations[target_relation] is not None:
+                print("extending frontier with rel:" , target_relation , "-", obj1.relations[target_relation].obj_label)
+                front.append(obj1.relations[target_relation])
 
+        print("after extending the front: ", len(front))
+        for object in front:
+            print(obj1_label , target_relation , object.obj_label)
 
+    def query(self, obj1, obj2):
+
+        print("\n\nQuerying relation between", obj1, obj2)
+
+        relations = {}
+        if self.queryRelation(obj1, obj2, "above"):
+            relations["above"] = True
+        if self.queryRelation(obj1, obj2, "left"):
+            relations["left"] = True
+        if self.queryRelation(obj1, obj2, "right"):
+            relations["right"] = True
+        if self.queryRelation(obj1, obj2, "bottom"):
+            relations["bottom"] = True
+
+        print("Relation between", obj1, "and", obj2)
+        if len(relations) == 0:
+            print("I don't know.")
+        else:
+            for r in relations:
+                print(r, "= true")
 
 def create_input_image(input):
     obj1 = Object(input.obj1)
@@ -163,26 +172,25 @@ def create_input_image(input):
 
 # The fork is to the left of the plate. The plate is to the left of the knife.
 def test1():
-    pd = ImageryArch()
+    imageArch = ImageryArch()
 
-    pd.stm.addImage(create_input_image(Input("left", "fork", "plate")))
-    pd.stm.addImage(create_input_image(Input("left", "plate", "knife")))
+    imageArch.stm.addImage(create_input_image(Input("left", "fork", "plate")))
+    imageArch.stm.addImage(create_input_image(Input("left", "plate", "knife")))
 
-    pd.query("fork", "knife")
+    imageArch.query("fork", "knife")
 
     print("\nTest 1 finished")
 
 
 # The fork is to the left of the plate. The plate is above the napkin
 def test2():
-    pd = ImageryArch()
+    imageArch = ImageryArch()
 
-    pd.stm.addImage(create_input_image(Input("left", "fork", "plate")))
-    pd.stm.addImage(create_input_image(Input("above", "plate", "napkin")))
+    imageArch.stm.addImage(create_input_image(Input("left", "fork", "plate")))
+    imageArch.stm.addImage(create_input_image(Input("above", "plate", "napkin")))
 
-    pd.query("fork", "napkin")
-
+    imageArch.query("fork", "napkin")
     print("\nTest 2 finished")
 
 if __name__ == "__main__":
-    test2()
+    test1()
